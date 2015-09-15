5319acd539e112a92a2db62605f42e506a28d09e
eryup <eric.yup1@gmail.com>
2015-09-15 11:36:18 -0700

    chemotaxis

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  int bacSize=30;
  Bacteria [] squad;
 // int step=8;
- void setup()   
+ public void setup()   
  {     
  	size(800,800);
  	squad=new Bacteria[500];
@@ -10,7 +26,7 @@
  		squad[i] = new Bacteria();
  	}
  }   
- void draw()   
+ public void draw()   
  {    
  	background(50);
  	for (int i=0;i<squad.length;i++)
@@ -29,14 +45,23 @@
  		myY=400;
  		//bacteriaColor=1;
  	}
- 	void move()
+ 	public void move()
  	{
  		myX=myX+(int)(Math.random()*5)-2;
  		myY=myY+(int)(Math.random()*5)-2;
  	}
- 	void show()
+ 	public void show()
  	{
  		fill(bacteriaColor[0],bacteriaColor[1],bacteriaColor[2],bacteriaColor[3]);
  		ellipse(myX, myY, bacSize, bacSize);
  	}
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
